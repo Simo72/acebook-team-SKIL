eaba8d64251d3edeb82bf811a0e1b7c96b128a6b
Simon Ashbery <si.ashbery@gmail.com>
2017-06-26 16:17:38 +0100

    Adds comment properties to db schema - t.string "text" - t.integer "post_id"

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20170626103214) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
