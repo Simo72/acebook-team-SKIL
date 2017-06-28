a1b795cac903561d712e263f2a3c6a5c3f5a8871
Lubos Michalic <lnmichalic@gmail.com>
2017-06-28 19:32:27 +0100

    Implement code so users can like posts   - Create Like Model   - Add Like Button in views   - Create relations in db between posts, users and likes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+
+  before_action :require_login, only: [:create, :new]
+
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @like = Like.create(like_params)
+    redirect_to posts_url
+  end
+
+  private
+
+  def like_params
+    params.permit(:user_id, :post_id)
+  end
+
+end
